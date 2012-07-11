5aa60a4d165c05b32c9c4d8a8194bacde4107f05
Josh Hoak <jrhoak@gmail.com>
2012-07-11 09:41:42 -0600

    Adding test data.  Initial work on th movetree tests.

--- src/sgf/movetree.js
+++ src/sgf/movetree.js
@@ -25,35 +25,48 @@ var enums = otre.enums;
  *  ]
  * }
  *
- * Thus, each move is an object with two properties: tokens and moves, the
- * latter of which is a list to capture the idea of multiple variations.
+ * If you are familiar with the SGF format, this should look very similar to the
+ * actual SGF format, and is easily converten back to a SGF. And so, The
+ * MoveTree is a simple wrapper around the parsed SGF.
  *
- * The MoveTree is a simple wrapper around the parsed SGF.
+ * Each move is an object with two properties: tokens and moves, the
+ * latter of which is a list to capture the idea of multiple variations.
  */
 otre.rules.movetree = {
   // Create an empty MoveTree
   getInstance: function() {
-    return new MoveTree(new MoveNode());
+    return new MoveTree(this._createNewMove());
   },
 
   // Create a MoveTree from an SGF.
   getFromSgfString: function(sgfString) {
-    return new MoveTree(
-      otre.sgf.parser.parse($.trim(sgfString)));
+    return new MoveTree(otre.sgf.parser.parse($.trim(sgfString)));
+  },
+
+  _createNewMove: function() {
+    return { tokens: {}, moves: [] };
   }
 };
 
 // A MoveTree is a history (a tree) of the past moves played.
-// The movetree is (usually) a processed parsed SGF.
+// The movetree is (usually) a processed parsed SGF, but could 
 //
 // The tree itself is tree structure made out of MoveNodes.
 var MoveTree = function(parsedSgf) {
   // This could be called the 'rootMove'.
-  this.parsedSgf = parsedSgf;
-  this.moveHistory = [];
+  this._parsedSgf = parsedSgf;
+
+  // The moveHistory serves two purposes -- it allows travel backwards (i.e.,
+  // up the tree), and it gives the current move, which is the last move in the
+  // array.
+  this._moveHistory = [].push(this._parsedSgf);
 };
 
 MoveTree.prototype = function() {
+  getCurrentMove: function() {
+    return this._moveHistory[this._moveHistory.length() - 1];
+  },
+
   //TODO
   addMove: function() {},
   //TODO
---
+++ src/sgf/movetree_test.js
@@ -0,0 +1,10 @@
+var movetree = otre.rules.movetree;
+var testsgfs = otre.testdata.sgfs;
+
+otre.movetree_test = function() {
+  test("simple parsing a token retrieval", function() {
+    //TODO(jh): finish this
+    equal(true, true, "");
+    ok(true, "");
+  });
+};
