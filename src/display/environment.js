ab2f9b2bbd607f7b042d4bde28a4c9229cabf024
Josh Hoak <jrhoak@gmail.com>
2012-08-16 16:50:16 -0700

    oeu

--- src/display/environment.js
+++ src/display/environment.js
@@ -10,12 +10,57 @@ otre.display.environment = {
   getInitialized: function(options) {
     return new (GuiEnvironment(options)).initialize();
   },
+
+  // Get the bounding box of a sidebar.
+  _getSidebarBox: function(displayType, divBox, direction) {
+    var dirs = enums.directions,
+        dispt = enums.displayTypes,
+        top = divBox.topLeft.y,
+        bot = divBox.botRight.y,
+        left = divBox.topLeft.x,
+        right = divbox.botRight.x;
+    if (direction === dirs.LEFT) {
+      // LEFT not yet used, so let width = 0
+      right = divBox.topLeft.x;
+    } else if (direction === dirs.RIGHT) {
+      // RIGHT not yet used, so let width = 0
+      left = divBox.botRight.x;
+    } else if (direction === dirs.TOP) {
+      bot = divBox.topLeft.y
+      if (displayType === dispt.EXPLAIN_BOARD) {
+        bot = 0.05 * divBox.height;
+      }
+    } else if (direction === dirs.BOT) {
+      top = divBox.botRight.y
+      if (displayType === dispt.EXPLAIN_BOARD) {
+        top = divBox.height * 0.95;
+      }
+    } else {
+      return otre.display.bbox(util.point(0,0), 0, 0);
+    }
+
+    return otre.display.bboxFromPts(
+        util.point(left, top), util.point(right, bot));
+  },
+
+  _getGoBoardBox: function(bbox, cropbox) {
+    var side = otre.math.min(bbox.height, bbox.width),
+        newLeft = bbox.topLeft.x + (bbox.width - side) / 2,
+        newTop = bbox.topLeft.y + (bbox.height - side) / 2;
+    return otre.display.bbox(util.point(left, top), side, side);
+  },
+
+  _getGoBoardLineBox: function(boardBox, cropbox) {
+    var xPoints = cropbox.width,
+        yPoints = cropbox.height,
+        minPoints = otre.math.min(xPoints, yPoints);
+  }
 };
 
 var GuiEnvironment = function(options) {
   this.divId = options.divId;
   this.displayType = options.displayType || enums.displayTypes.SIMPLE_BOARD;
-  this.cropbox = options.cropbox || this.getCropboxFromRegion(
+  this.cropbox = options.cropbox || otre.display.getCropbox(
       options.displayRegion, options.intersections);
 
   // We allow the divHeight and divWidth to be specified explicitly, primarily
@@ -28,6 +73,7 @@ GuiEnvironment.prototype = {
   // Initialize the internal variables that tell where to place the go broard.
   initialize: function() {
     var display = otre.display,
+        env = display.environment,
         divHeight = this.divHeight,
         divWidth  = this.divWidth,
         cropbox   = this.cropbox,
@@ -39,17 +85,22 @@ GuiEnvironment.prototype = {
             util.point(0, 0), // top left
             util.point(divWidth, divHeight)), // bottom rigt
 
-        topBar = this._getSidebarBox(displayType, divBox, dirs.TOP),
-        leftBar = this._getSidebarBox(displayType, divBox, dirs.LEFT),
-        bottomBar = this._getSidebarBox(displayType, divBox, dirs.BOTTOM),
-        rightBar = this._getSidebarBox(displayType, divBox, dirs.RIGHT),
+        topBar = env._getSidebarBox(displayType, divBox, dirs.TOP),
+        leftBar = env._getSidebarBox(displayType, divBox, dirs.LEFT),
+        bottomBar = env._getSidebarBox(displayType, divBox, dirs.BOTTOM),
+        rightBar = env._getSidebarBox(displayType, divBox, dirs.RIGHT),
 
-        goBoardBox = this._getBoardBox(
-            topBar.botRight.y, leftBar.botRight.x,
-            bottomBar.topLeft.y, rightBar.topLeft.x),
+        xPoints = cropbox.width,
+        yPoints = cropbox.height,
+        minSpacing = boardSide / (otre.math.min(xPoints, yPoints) + 1/3),
+
+        goGoBoardBox = env._getGoBoardBox(otre.bboxFromPts(
+            util.point(leftBar.botRight.x, topBar.botRight.y),
+            util.point(rightBar.topLeft.x, bottomBar.topLeft.y)),
+            cropbox),
 
         // TODO
-        goBoardLineBox = this._getGoBoardLineBox(divBox, cropbox);
+        goBoardLineBox = env._getGoBoardLineBox(divBox, cropbox);
 
     // Move the bars based on the leftover height and width.
     topBar.topLeft.y = topBar.topLeft.y + 
@@ -71,55 +122,6 @@ GuiEnvironment.prototype = {
     return this.initialize();
   },
 
-  // Get the bounding box of the side.
-  _getSidebarBox: function(displayType, divBox, direction) {
-    var dirs = enums.directions,
-        dispt = enums.displayTypes,
-        top = divBox.topLeft.y,
-        bot = divBox.botRight.y,
-        left = divBox.topLeft.x,
-        right = divbox.botRight.x;
-
-      // LEFT and RIGHT not yet used, so let width = 0
-    if (direction === dirs.LEFT) {
-      right = divBox.topLeft.x;
-    } else if (direction === dirs.RIGHT) {
-      left = divBox.botRight.x;
-    } else if (direction === dirs.TOP) {
-      bot = divBox.topLeft.y
-      if (displayType === dispt.EXPLAIN_BOARD) {
-        bot = 0.05 * divBox.height;
-      }
-    } else if (direction === dirs.BOT) {
-      top = divBox.botRight.y
-      if (displayType === dispt.EXPLAIN_BOARD) {
-        top = divBox.height * 0.95;
-      }
-    } else {
-      return otre.display.bbox(util.point(0,0), 0, 0);
-    }
-
-    return otre.display.bboxFromPts(
-        util.point(left, top), util.point(right, bot));
-  },
-
-  _getGoBoardBox: function(top, left, bot, right) {
-    var height = bot - top,
-        width = right - left,
-        side = otre.math.min(height, width),
-        heightRemain = height - side,
-        widthRemain = width - side,
-        newLeft = left + widthRemain / 2,
-        newRight = newLeft + side,
-        newTop = top + heightRemain / 2,
-        newBot = newTop + side;
-    return otre.display.bboxFromPts(
-        util.point(left, top), util.point(right, bot));
-  },
-
-  _getGoBoardLineBox: function(boardBox, cropbox) {
-
-  },
 
   _debugDrawAll: function() {
     this.divBox.draw(this.paper, 'red');
